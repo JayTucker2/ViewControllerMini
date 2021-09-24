119b45357a06069f4edf4df9735c165436aa9d7c
JayTucker2 <prosnipa1221@gmail.com>
2021-09-24 07:57:32 -0500

    Initial Commit

---
+++ ViewControllerMini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewControllerMini
+//
+//  Created by JAYLAN TUCKER on 9/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
